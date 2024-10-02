d407c8c7b72bd8774a896907fa3cebf9900cc96b
NullByte3 <email@address.com>
2024-10-02 19:03:51 +0300

    feat: Deleted test.py as it was useless; Added get_airports_by_continent & haversine formula, sql files.

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+from math import radians, cos, sin, asin, sqrt
 
 db = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
@@ -8,6 +9,26 @@ db = mysql.connector.connect(
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
     )
+
+def get_airports_by_continent(db, continent):
+    cursor = db.cursor()
+    if continent == 'AN':
+        cursor.execute("SELECT name, latitude_deg, longitude_deg FROM airport WHERE continent = 'AN' ORDER BY RAND() LIMIT 5")
+    else:
+        cursor.execute("SELECT name, latitude_deg, longitude_deg FROM airport WHERE continent = %s AND type = 'large_airport' ORDER BY RAND() LIMIT 5", (continent,))
+    return cursor.fetchall()
+
+# haversine formula to calculate distance between two GPS points
+# Read more at: https://en.wikipedia.org/wiki/Haversine_formula
+# - NullByte3
+def haversine(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371
+    return c * r
 
 def get_db():
     return db
---
+++ db/tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS `airport` (
+    `id` int(11) NOT NULL,
+    `ident` varchar(40) NOT NULL,
+    `type` varchar(40) DEFAULT NULL,
+    `name` varchar(40) DEFAULT NULL,
+    `latitude_deg` double DEFAULT NULL,
+    `longitude_deg` double DEFAULT NULL,
+    `elevation_ft` int(11) DEFAULT NULL,
+    `continent` varchar(40) DEFAULT NULL,
+    `iso_country` varchar(40) DEFAULT NULL,
+    `iso_region` varchar(40) DEFAULT NULL,
+    `municipality` varchar(40) DEFAULT NULL,
+    `scheduled_service` varchar(40) DEFAULT NULL,
+    `gps_code` varchar(40) DEFAULT NULL,
+    `iata_code` varchar(40) DEFAULT NULL,
+    `local_code` varchar(40) DEFAULT NULL,
+    `home_link` varchar(40) DEFAULT NULL,
+    `wikipedia_link` varchar(40) DEFAULT NULL,
+    `keywords` varchar(40) DEFAULT NULL,
+    PRIMARY KEY (`ident`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `country` (
+    `iso_country` varchar(40) NOT NULL,
+    `name` varchar(40) DEFAULT NULL,
+    `continent` varchar(40) DEFAULT NULL,
+    `wikipedia_link` varchar(40) DEFAULT NULL,
+    `keywords` varchar(40) DEFAULT NULL,
+    PRIMARY KEY (`iso_country`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
